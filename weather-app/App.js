55f1a42fb3beee322c395fc1326c60987daf2e4b
jihoonyou <pianoetvoix@gmail.com>
2019-04-16 14:22:34 +0900

    Add geolocation

--- weather-app/App.js
+++ weather-app/App.js
@@ -5,15 +5,30 @@ import Weather from './Weather';
 export default class App extends React.Component {
   state = {
     isLoaded: false
+  };
+  componentDidMount() {
+     navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          isLoaded: true
+        });
+      },
+      err => {
+        this.setState({
+          error: err
+        })
+      }
+    );
   }
   render() {
-    const { isLoaded } = this.state;
+    const { isLoaded, error } = this.state;
     return (
       <View style={styles.container}>
         <StatusBar hidden={true} />
         { isLoaded ? null : 
         <View style={styles.loading}>
-<Text style={styles.loadingText}>Getting the weather</Text>
+          <Text style={styles.loadingText}>Getting the weather</Text>
+          {error ? <Text style={styles.errorText}>{error}</Text> : null}
         </View>}
       </View>
     );
@@ -24,6 +39,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff"
+  },
+  errorText: {
+    color: "red",
+    backgroundColor: "transparent",
+    marginBottom: 40
   },
   loading: {
     flex: 1,
