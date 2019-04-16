88f22906f711bb3ba42279bbda26b100117f2c20
jihoonyou <pianoetvoix@gmail.com>
2019-04-16 10:39:17 +0900

    Add loading view

--- weather-app/App.js
+++ weather-app/App.js
@@ -2,11 +2,17 @@ import React from 'react';
 import { StyleSheet, Text, View, ActivityInidcator } from 'react-native';
 
 export default class App extends React.Component {
+  state = {
+    isLoaded: false
+  }
   render() {
+    const { isLoaded } = this.state;
     return (
       <View style={styles.container}>
-      <ActivityInidcator />
-        <Text>Open up App.js to start working on your app!</Text>
+        { isLoaded ? null : 
+        <View style={styles.loading}>
+<Text style={styles.loadingText}>Getting the weather</Text>
+        </View>}
       </View>
     );
   }
@@ -15,8 +21,17 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff"
   },
+  loading: {
+    flex: 1,
+    backgroundColor: "#FDF6AA",
+    justifyContent: "flex-end",
+    paddingLeft: 25
+  },
+    loadingText: {
+    fontSize: 38,
+    marginBottom: 100
+  }
+
 });
