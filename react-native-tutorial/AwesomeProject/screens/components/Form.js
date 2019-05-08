6fcc5f3d48770524bbe7ac409b6593a35e441da4
jihoonyou <pianoetvoix@gmail.com>
2019-05-08 15:32:32 +0900

    Add add userLogin

--- react-native-tutorial/AwesomeProject/screens/components/Form.js
+++ react-native-tutorial/AwesomeProject/screens/components/Form.js
@@ -1,22 +1,42 @@
 import React, {Component} from 'react';
 import { StyleSheet,View, Button } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
-
+import firebase from 'firebase';
 export default class Form extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = ({
+      email: '',
+      password: ''
+    });
+  }
+
+  userLogin = (email, password) => {
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+    });
+  }
+  
   render() {
     return (
       <View style={styles.form}>
         <TextInput style={styles.textinput} placeholder="Email"
         underlineColorAndroid='rgba(0, 0, 0, 0)'
         placeholderTextColor= "#fff"
+        onChangeText={(email) => this.setState({ email })}
         ></TextInput>
         <TextInput style={styles.textinput} placeholder="Password"
         secureTextEntry
         underlineColorAndroid='rgba(0, 0, 0, 0)'
         placeholderTextColor= "#fff"
+        onChangeText={(password) => this.setState({ password })}
         ></TextInput>
         <Button title="Login" 
-        onPress={() => alert('button pressed')}/>
+        onPress={() => this.userLogin(this.state.email, this.state.password)}/>
       </View>
     );
   }
